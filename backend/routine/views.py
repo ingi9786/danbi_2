91d71a86f76c58b45d2227edb8eb93ac1025461e
ingi9786 <ingi9786@naver.com>
2022-09-20 16:45:45 +0900

    [#20][fix]: Modify response format

--- backend/routine/views.py
+++ backend/routine/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import generics, mixins
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 from .models import Routine, RoutineDay, RoutineResult
 from .serializers import RoutineSerializer, RoutineDaySerializer, RoutineResultSerializer
@@ -21,9 +22,9 @@ class RoutineListCreateAPIView(ListQuerySetMixin, generics.ListCreateAPIView):
         serializer = RoutineSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(uid=user.id)
-            msg =  {"msg":"You have successfully created the routine.", "status": "ROUTINE_CREATE_OK"}
             return Response({"data":{"routine_id": serializer.data.get('id', None)},
-                             "message":msg
+                             "message": "You have successfully created the routine.",
+                             "status": status.HTTP_201_CREATED
                             })
  
     def list(self, request, *args, **kwargs):
@@ -34,13 +35,17 @@ class RoutineListCreateAPIView(ListQuerySetMixin, generics.ListCreateAPIView):
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         
-        msg = {"msg":"You have successfully lookup the routines.", "status": "ROUTINE_LIST_OK"}
+        
+        msg = "You have successfully lookup the routines."
         if not queryset.exists():
-            return Response({"message": msg})
+            return Response({"message": msg,
+                             "status": status.HTTP_200_OK
+                            })
 
         serializer = self.get_serializer(queryset, many=True)
         return  Response({"data": serializer.data,
-                          "message": msg
+                          "message": msg,
+                          "status": status.HTTP_204_NO_CONTENT
                         })
 
 # Detail view
@@ -58,12 +63,10 @@ class RoutineDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-        msg = {"msg":"You have successfully lookup the routine.", "status": "ROUTINE_DETAIL_OK"}
-        return Response({"data":{
-                                "account_id": self.request.user.id,
-                                "routine_id": serializer.data
-                                },
-                        "message":msg
+        return Response({"data":{"account_id": self.request.user.id,
+                                 "routine_id": serializer.data},
+                        "message": "You have successfully lookup the routine.",
+                        "status": status.HTTP_200_OK
                         })
 
     def update(self, request, *args, **kwargs):
@@ -72,17 +75,18 @@ class RoutineDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     
         if serializer.is_valid(raise_exception=True):
             serializer.save(rid=instance.id)
-            msg = {"msg":"You have successfully updated the routine.", "status": "ROUTINE_UPDATE_OK"}
             return Response({"data": {"routine_id": serializer.data.get('id', None)}, 
-                             "message": msg
+                             "message": "You have successfully updated the routine.",
+                             "status": status.HTTP_200_OK
                             })
             
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         view_utils.logical_delete_routine(instance)
-        msg = {"msg":"You have successfully deleted the routine.", "status": "ROUTINE_DELETE_OK"}
+
         return Response({"data":{"routine_id": instance.id}, 
-                         "message": msg
+                         "message": "You have successfully deleted the routine.",
+                         "status": status.HTTP_200_OK
                         })
 
 
