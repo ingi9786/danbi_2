4f33682c2bf38da66e7d7bc784f60c8817d5d6aa
ingi9786 <ingi9786@naver.com>
2022-10-13 16:26:01 +0900

    [#40][feat]: Add collectstatic

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -4,6 +4,7 @@ cd /app/
 
 /opt/venv/bin/python manage.py migrate --noinput
 /opt/venv/bin/python manage.py createsuperuser --email ${SUPERUSER_EMAIL} --noinput || true
+/opt/venv/bin/python manage.py collectstatic --noinput
 
 # django 3.0부터 createsupuser --noinput으로 
 # 환경변수로 REQUIRED_FIEDLS의 필드들이 DJANGO_SUPERUSER_UPPER FIELD NAME이 설정되어있으면 non-interactive한 실행 시 자동으로 슈퍼유저를 생성.
